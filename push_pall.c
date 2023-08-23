2472a9547d7cbb97741e88f0fa22175fe0745e60
Aya-oba <mbackeousmane31@gmail.com>
2023-08-23 02:48:56 +0000

    0. push, pall

--- push_pall.c
+++ push_pall.c
@@ -1,66 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "monty.h"
+/**
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_push(stack_t **head, unsigned int counter)
+{
+	int n, j = 0, flag = 0;
 
-#define STACK_SIZE 100
-
-int stack[STACK_SIZE];
-int top = -1; // Index of the top element in the stack
-
-// Push an element onto the stack
-void push(int value) {
-    if (top >= STACK_SIZE - 1) {
-        fprintf(stderr, "Stack overflow!\n");
-        exit(EXIT_FAILURE);
-    }
-    stack[++top] = value;
-}
-
-// Print all elements in the stack from top to bottom
-void pall() {
-    for (int i = top; i >= 0; i--) {
-        printf("%d\n", stack[i]);
-    }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    FILE *file = fopen(argv[1], "r");
-    if (!file) {
-        fprintf(stderr, "Error opening file: %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-
-    char line[100];
-    int line_number = 1;
-
-    while (fgets(line, sizeof(line), file)) {
-        char opcode[100];
-        int value;
-        
-        if (sscanf(line, "%s %d", opcode, &value) == 2) {
-            if (strcmp(opcode, "push") == 0) {
-                push(value);
-            } else {
-                fprintf(stderr, "Unknown opcode: %s\n", opcode);
-                fclose(file);
-                return EXIT_FAILURE;
-            }
-        } else if (strcmp(opcode, "pall") == 0) {
-            pall();
-        } else {
-            fprintf(stderr, "Invalid instruction on line %d\n", line_number);
-            fclose(file);
-            return EXIT_FAILURE;
-        }
-
-        line_number++;
-    }
-
-    fclose(file);
-    return EXIT_SUCCESS;
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
+		{
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
